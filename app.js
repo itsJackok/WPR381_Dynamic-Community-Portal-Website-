a6c4da3d1939263da2f22c6be35af96f6c655975
itsJackok <150603497+itsJackok@users.noreply.github.com>
2025-05-14 02:40:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const pageRoutes = require('./routes/pageRoutes');
 
-const pageRoutes = require('./routes/pageRoutes'); 
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', pageRoutes);
 
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(` Server running at http://localhost:${PORT}`);
+});
